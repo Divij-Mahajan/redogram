6330df8355de471d3ec2dfb8a4067fa688d83577
Divij Mahajan <d.mahajan2004@gmail.com>
2024-12-16 20:38:46 +0530

    congifuration updates

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -11,14 +11,28 @@ export default function Header({total,current,user,setCurrent}:Props) {
     let l=[
     ]
     for(let i=0;i<total;i++){
-        l.push(".")
+        l.push("_")
     }
     return <hstack alignment='center middle' width="100%" padding='small'>
             <spacer grow/>
+            <spacer />
         <hstack gap='small'>
-            {l.map((r)=>{
-                return <text>.</text>
+            {l.map((r,i)=>{
+                if(i== current){
+                    return <vstack>
+                    <text size='xxlarge' style='heading' color={i<user-1?"#FF3F18":"#FFFFFF"}>_</text>
+                    <text size='xxlarge' style='heading' color={i<user-1?"#FF3F18":"#FFFFFF"}>^</text>
+                    </vstack>
+                }
+                return <text size='xxlarge' style='heading' color={i<user-1?"#FF3F18":"#FFFFFF"}>_</text>
             })}
+             <vstack alignment='center'>
+                <spacer grow></spacer>
+                    <icon name="contest" color={user==total+1?"#FF3F18":"#FFFFFF"}></icon>
+                    {current==total?
+                    <text size='xxlarge' style='heading' color="#FF3F18">^</text>:<></>
+                    }
+            </vstack>
         </hstack>
         <spacer grow/>
         <hstack gap='small'>
--- src/pages/Configure.tsx
+++ src/pages/Configure.tsx
@@ -27,6 +27,23 @@ export default function Configure({userId,postId,isModerator,setScreen,ui,redis}
             })
         }
     )
+    let rewardForm=useForm(
+        {
+        fields: [
+            {
+            type: 'string',
+            name: 'phrase',
+            label: 'Reward Phrase :',
+            },
+        ],
+        },
+        (values) => {
+            if(values){
+                setReward(values.phrase||"")
+                setEntered(true)
+            }
+        }
+    )
 
     function shuffle() {
         let a="abcdefghijklmnopqrstuvwxyz0123456789";
@@ -47,6 +64,8 @@ export default function Configure({userId,postId,isModerator,setScreen,ui,redis}
         }
         return z;
     }
+    const [reward, setReward] = useState("Click here to add text.")
+    const [entered, setEntered] = useState(false)
     
     return <vstack alignment='center' height="100%" width="100%" gap='small'>
         
@@ -67,11 +86,27 @@ export default function Configure({userId,postId,isModerator,setScreen,ui,redis}
                 await redis.set(postId+"live",JSON.stringify(true));
                 await redis.set(postId+"number",JSON.stringify(questions.length));
                 await redis.set(postId+"rounds",JSON.stringify(z));
+                if(entered){
+                    await redis.set(postId+"reward",JSON.stringify(reward));
+                }else{
+                    await redis.set(postId+"reward",JSON.stringify("Keep Up the good work!!"));
+                }
                 ui.showToast("Success! Please reload.")
             }}>Submit</button>
         </hstack>
 
         <vstack alignment='center' height="100%" width="100%" gap='small'>
+            <hstack alignment='start' width="100%">
+                <spacer></spacer><spacer></spacer><spacer></spacer><spacer></spacer><spacer></spacer>
+                <text size='xlarge' style='heading' color='#AA7777'>Reward Phrase :</text>
+                <spacer></spacer>
+                <spacer></spacer>
+                <hstack padding='small' border='thin' borderColor='white' cornerRadius='small' width="70%" onPress={() => {
+                            ui.showForm(rewardForm);
+                        }}>
+                       <text width="100%" color={entered?"#FFFFFF":"#888888"}>{reward}</text>
+                    </hstack>
+            </hstack>
             <hstack alignment='start' width="100%">
                 <spacer></spacer><spacer></spacer><spacer></spacer><spacer></spacer><spacer></spacer>
                 <text size='xlarge' style='heading' color='#AA7777'>Phrases:</text>
@@ -80,7 +115,17 @@ export default function Configure({userId,postId,isModerator,setScreen,ui,redis}
                     <spacer></spacer><spacer></spacer><spacer></spacer><spacer></spacer><spacer></spacer>
                 <vstack  gap="small" alignment='start top'>
                     {questions.map((q,i)=>{
-                        return <text size='large'>{i+1}. {q}</text>
+                        return <hstack width="100%">
+
+                        <text size='large'>{i+1}. {q}</text>
+                        <spacer grow></spacer>
+                        <spacer grow></spacer>
+                        <spacer grow></spacer>
+                        <spacer grow></spacer>
+                        <icon name='delete' onPress={()=>{
+                            setQuestions([...questions.slice(0,i),...questions.slice(i+1)])
+                        }}></icon>
+                        </hstack>
                     })}
                     <spacer></spacer><spacer></spacer>
                     <hstack border='thin' borderColor='white' cornerRadius='medium'>
